707f6d584793d70cdb492493259a50050659847d
akb2 <akb2-online@ya.ru>
2026-08-20 23:09:06 +0400

    Changed message

--- src/pages/workflow/data.ts
+++ src/pages/workflow/data.ts
@@ -1,3 +1,3 @@
 export const CHECKING_INTERVAL = 500;
 
-export const NO_BUTTONS_NOTIFICATION = "No buttons available for this list";
+export const NO_BUTTONS_NOTIFICATION = "No actions available for the task";

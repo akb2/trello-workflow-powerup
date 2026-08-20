26b64b05cfc8506878854e478845ee4149452cfc
akb2 <akb2-online@ya.ru>
2026-08-20 20:59:33 +0400

    Return button

---
+++ src/utils/return-to-clarification-handler.ts
@@ -0,0 +1,4 @@
+import { TrelloPowerUpContext } from "../models/trello-power-up-context";
+
+export const returnToClarificationHandler = async (t: TrelloPowerUpContext) => {
+}
--- src/workflow/buttons.ts
+++ src/workflow/buttons.ts
@@ -2,6 +2,7 @@ import { ButtonCondition } from "../models/button-condition";
 import { ListType } from "../models/list-type";
 import { TrelloButton } from "../models/trello-button";
 import { lucideIcon } from "../utils/lucide-icon";
+import { returnToClarificationHandler } from "../utils/return-to-clarification-handler";
 import { startDevelopmentHandler } from "../utils/start-development-handler";
 
 export const BUTTONS: TrelloButton[] = [
@@ -16,5 +17,14 @@ export const BUTTONS: TrelloButton[] = [
     condition: ButtonCondition.Edit,
     listType: ListType.ReadyForDevelopment,
     callback: startDevelopmentHandler.bind(null),
+  },
+
+  {
+    theme: "danger",
+    text: "Return for Clarification",
+    icon: lucideIcon('square-dashed-text'),
+    condition: ButtonCondition.Edit,
+    listType: ListType.ReadyForDevelopment,
+    callback: returnToClarificationHandler.bind(null),
   }
 ];

b7e71bbb2b63652d10db360baebc8505a7f6e542
akb2 <akb2-online@ya.ru>
2026-08-20 23:12:21 +0400

    Removed code review

--- src/models/list-type.ts
+++ src/models/list-type.ts
@@ -2,7 +2,6 @@ export enum ListType {
   BackLog = 'Backlog',
   ReadyForDevelopment = 'Ready for Development',
   InDevelopment = 'In Development',
-  ReadyForCodeReview = 'Ready for Code Review',
   InCodeReview = 'In Code Review',
   ReadyForTesting = 'Ready for Testing',
   InTesting = 'In Testing',

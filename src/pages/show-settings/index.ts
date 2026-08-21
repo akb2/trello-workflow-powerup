2c91f3b59b8bd3f6a14e680e705dc11fc8f14976
akb2 <akb2-online@ya.ru>
2026-08-21 15:31:46 +0400

    Changed url

--- src/pages/show-settings/index.ts
+++ src/pages/show-settings/index.ts
@@ -1,7 +1,12 @@
 import { TrelloPowerUpContext } from "../../models/trello-power-up-context";
 
+
+const workflowUrl = new URL(import.meta.env.BASE_URL + "src/pages/settings/index.html", window.location.origin);
+
+workflowUrl.searchParams.set("v", Date.now().toString());
+
 export const showSettingsConnector = (t: TrelloPowerUpContext): Promise<unknown> => t.popup({
   title: "Workflow Settings",
-  url: "./index.html",
+  url: t.signUrl(workflowUrl.href),
   height: 400,
 });

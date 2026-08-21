290e6345f23afe0ad0d8b0cdab0ee3ef97bd86b9
akb2 <akb2-online@ya.ru>
2026-08-21 15:44:16 +0400

    Updated urls

--- src/pages/card-back-section/index.ts
+++ src/pages/card-back-section/index.ts
@@ -1,16 +1,16 @@
 import { TrelloCardBackSection } from "../../models/trello-card-back-section";
 import { TrelloPowerUpContext } from "../../models/trello-power-up-context";
 
-const workflowUrl = new URL(import.meta.env.BASE_URL + "src/pages/workflow/index.html", window.location.origin);
+const url = new URL(import.meta.env.BASE_URL + "src/pages/workflow/index.html", window.location.origin);
 
-workflowUrl.searchParams.set("v", Date.now().toString());
+url.searchParams.set("v", Date.now().toString());
 
 export const cardBackSectionConnector = async (t: TrelloPowerUpContext): Promise<TrelloCardBackSection> => ({
   title: "Workflow",
   icon: "https://cdn.jsdelivr.net/npm/lucide-static@1.31.0/icons/workflow.svg",
   content: {
     type: "iframe",
-    url: t.signUrl(workflowUrl.href),
+    url: t.signUrl(url.href),
     height: 0,
   },
 })

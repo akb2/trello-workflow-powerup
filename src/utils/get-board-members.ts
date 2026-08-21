5455107e7d28e349ed1871e6723d12cf3c52916a
akb2 <akb2-online@ya.ru>
2026-08-21 17:54:18 +0400

    Changed getMembers data

--- src/utils/get-board-members.ts
+++ src/utils/get-board-members.ts
@@ -1,8 +1,27 @@
 import { TrelloMember } from "../models/trello-member";
 import { TrelloPowerUpContext } from "../models/trello-power-up-context";
+import { getAuth } from "./get-auth";
 
 export const getBoardMembers = async (t: TrelloPowerUpContext): Promise<TrelloMember[]> => {
-  const { members } = await t.board("members");
+  const { board } = t.getContext();
+  const auth = await getAuth(t);
 
-  return members;
+  const params = new URLSearchParams({
+    ...Object.fromEntries(auth),
+    fields: [
+      "id",
+      "fullName",
+      "username",
+      "initials",
+      "avatarUrl",
+    ].join(","),
+  });
+
+  const response = await fetch(`https://api.trello.com/1/boards/${board}/members?${params}`);
+
+  if (!response.ok) {
+    throw new Error("Failed to get board members");
+  }
+
+  return response.json();
 };

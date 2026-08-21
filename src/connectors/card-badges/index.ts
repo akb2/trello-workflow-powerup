a8648f87d9590f35f313f0392d19f0a37af0f04c
akb2 <akb2-online@ya.ru>
2026-08-21 20:01:37 +0400

    Added assignee badge

--- src/connectors/card-badges/index.ts
+++ src/connectors/card-badges/index.ts
@@ -1,13 +1,34 @@
 import { TrelloBadgeColor } from "../../models/trello-badge-color";
 import { TrelloCardBadge } from "../../models/trello-card-badge";
 import { TrelloPowerUpContext } from "../../models/trello-power-up-context";
+import { getCardAssignee } from "../../utils/get-card-assignee";
+import { getMemberAvatarUrl } from "../../utils/get-member-avatar-url";
 import { getTaskNumber } from "../../utils/get-task-number";
 import { lucideIcon } from "../../utils/lucide-icon";
 
-export const cardBadgesConnector = async (t: TrelloPowerUpContext): Promise<TrelloCardBadge[]> => ([
-  {
-    text: await getTaskNumber(t),
-    icon: lucideIcon("hash"),
-    color: TrelloBadgeColor.Blue,
-  }
-]);
+export const cardBadgesConnector = async (t: TrelloPowerUpContext): Promise<TrelloCardBadge[]> => {
+  const [taskNumber, assignee] = await Promise.all([
+    getTaskNumber(t),
+    getCardAssignee(t),
+  ]);
+  const avatarUrl = assignee
+    ? getMemberAvatarUrl(assignee)
+    : null;
+
+  return [
+    ...(assignee
+      ? [{
+        text: avatarUrl ? "" : assignee?.fullName,
+        icon: avatarUrl ?? lucideIcon("user-round"),
+        color: avatarUrl ? TrelloBadgeColor.Blue : TrelloBadgeColor.LightGray,
+        monochrome: !avatarUrl,
+      }]
+      : []
+    ),
+    {
+      text: taskNumber,
+      icon: lucideIcon("hash"),
+      color: TrelloBadgeColor.Blue,
+    }
+  ];
+};

09c81d9dd465e9888ff2e01432552a243000f0d7
akb2 <akb2-online@ya.ru>
2026-08-25 16:25:50 +0400

    Removed signUrl

--- src/connectors/show-settings/index.ts
+++ src/connectors/show-settings/index.ts
@@ -1,12 +1,11 @@
 import { TrelloPowerUpContext } from "../../models/trello-power-up-context";
 
-
 const url = new URL(import.meta.env.BASE_URL + "src/pages/settings/index.html", window.location.origin);
 
 url.searchParams.set("v", Date.now().toString());
 
 export const showSettingsConnector = (t: TrelloPowerUpContext): Promise<unknown> => t.popup({
   title: "Workflow Settings",
-  url: t.signUrl(url.href),
+  url: url.href,
   height: 400,
 });

d919da72695da67dd3b6debc1b796db70858bf71
akb2 <akb2-online@ya.ru>
2026-08-27 02:56:26 +0400

    Styled button

--- src/pages/authorization/index.ts
+++ src/pages/authorization/index.ts
@@ -1,16 +1,26 @@
 import { isDefined } from "@akb2/types-tools";
+import { buttonComponent } from "../../components/button/button";
 import { APP_OPTIONS } from "../../data/app-settings";
+import { lucideIcon } from "../../utils/lucide-icon";
 
 const t = window.TrelloPowerUp.iframe(APP_OPTIONS);
-const authorizeButton = document.getElementById("authorize-button");
+const rootContainer = document.getElementById("root-container");
+let renderPromise = Promise.resolve();
 
-if (!isDefined(authorizeButton)) {
-  throw new Error("Authorize button not found");
+if (!isDefined(rootContainer)) {
+  throw new Error("Root container not found");
 }
 
-authorizeButton.addEventListener(
-  "click",
-  async () => {
+/**
+ * Render funcs
+ */
+
+const render = async () => rootContainer.replaceChildren(await buttonComponent({
+  trelloContext: t,
+  text: "Authorize",
+  theme: "primary",
+  icon: lucideIcon("log-in"),
+  callback: async () => {
     const api = await t.getRestApi();
 
     await api.authorize({
@@ -18,5 +28,17 @@ authorizeButton.addEventListener(
     });
 
     await t.notifyParent("done");
-  },
-);
+  }
+}));
+
+const requestRender = (): Promise<void> => {
+  renderPromise = renderPromise.then(render);
+
+  return renderPromise;
+};
+
+/**
+ * Render flow
+ */
+
+t.render(requestRender);

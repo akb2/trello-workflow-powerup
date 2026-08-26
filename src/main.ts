e4d1fff37b3d78075dcbe2beef238834c82315a6
akb2 <akb2-online@ya.ru>
2026-08-27 02:45:03 +0400

    Implemented connectors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import { authorizationStatusConnector } from "./connectors/authorization-status";
 import { boardButtonsConnector } from "./connectors/board-buttons";
 import { cardBackSectionConnector } from "./connectors/card-back-section";
 import { cardBadgesConnector } from "./connectors/card-badges";
 import { cardDetailBadgesConnector } from "./connectors/card-detail-badges";
+import { showAuthorizationConnector } from "./connectors/show-authorization";
 import { showSettingsConnector } from "./connectors/show-settings";
 import { APP_OPTIONS } from "./data/app-settings";
 
 window.TrelloPowerUp.initialize({
+  "authorization-status": authorizationStatusConnector,
+  "show-authorization": showAuthorizationConnector,
   "board-buttons": boardButtonsConnector,
   "card-back-section": cardBackSectionConnector,
   "show-settings": showSettingsConnector,

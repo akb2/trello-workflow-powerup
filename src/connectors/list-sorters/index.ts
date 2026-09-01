a2b625178d7eecb148988ff96001e963310b83b6
akb2 <akb2-online@ya.ru>
2026-09-01 16:12:32 +0400

    Destructed

--- src/connectors/list-sorters/index.ts
+++ src/connectors/list-sorters/index.ts
@@ -1,3 +1,4 @@
+import { isDefined } from "@akb2/types-tools";
 import { CARD_PRIORITY_ORDER } from "../../data/card-priority-order";
 import { compareCardsPriority } from "../../data/compare-cards-priority";
 import { ListSorterOptions } from "../../models/list-sorter-options";
@@ -13,12 +14,12 @@ export const listSortersConnector = ({ }: TrelloPowerUpContext) => ([
       const cards = await Promise.all(
         opts.cards.map(
           async (card, manualIndex): Promise<SortableCard> => {
-            const settings = await getCardSettings(trelloContext, card.id);
+            const { priority } = await getCardSettings(trelloContext, card.id);
 
             return {
               card,
-              priority: settings.priority
-                ? CARD_PRIORITY_ORDER[settings.priority]
+              priority: isDefined(priority)
+                ? CARD_PRIORITY_ORDER[priority]
                 : Number.POSITIVE_INFINITY,
               manualIndex,
             };

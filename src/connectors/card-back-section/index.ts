424a9339801ac6002fdc67a4104a43c82bf4d2c7
akb2 <akb2-online@ya.ru>
2026-09-03 00:34:51 +0400

    Added anti cache

--- src/connectors/card-back-section/index.ts
+++ src/connectors/card-back-section/index.ts
@@ -1,7 +1,7 @@
 import { TrelloCardBackSection } from "../../models/trello-card-back-section";
 import { TrelloPowerUpContext } from "../../models/trello-power-up-context";
 
-const url = new URL(import.meta.env.BASE_URL + "src/pages/workflow/index.html", window.location.origin);
+const url = new URL(import.meta.env.BASE_URL + "src/pages/workflow/index.html?1", window.location.origin);
 
 url.searchParams.set("v", Date.now().toString());
 

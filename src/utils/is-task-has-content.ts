d574fe93bebedd36ac3eefe9c689da74ead42306
akb2 <akb2-online@ya.ru>
2026-09-02 22:36:11 +0400

    Fixed trim

--- src/utils/is-task-has-content.ts
+++ src/utils/is-task-has-content.ts
@@ -1,5 +1,7 @@
-export const isTaskHasContent = (card: { desc: string; attachments: unknown[]; badges: { comments: number } }): boolean => (
-  card.desc.trim().length > 0
+import { TrelloCard } from "../models/trello-card";
+
+export const isTaskHasContent = (card: TrelloCard): boolean => (
+  card.desc?.trim().length > 0
   || card.attachments.length > 0
   || card.badges.comments > 0
 );

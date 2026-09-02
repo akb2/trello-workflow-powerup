e692255e0569d6a50fbe19d9abceae3982c8a006
akb2 <akb2-online@ya.ru>
2026-09-02 21:24:48 +0400

    Added console

--- src/validators/detect-list.validator.ts
+++ src/validators/detect-list.validator.ts
@@ -39,7 +39,7 @@ export async function detectListValidator(trelloContext: TrelloPowerUpContext, c
     throw new Error("List type not found");
   }
 
-  console.log('akb2', card, listType, beforeListType);
+  console.log('akb2', card, lists, listType, beforeListType);
   switch (listType) {
     case ListType.BackLog:
       return true;
